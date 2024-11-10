101fc28fe16fd145d4099f4a0a3afc7aa5ed40fc
Tim <taehun112233@gmail.com>
2024-11-10 23:45:27 +0900

    Board Setting

--- 2048/main.py
+++ 2048/main.py
@@ -5,24 +5,37 @@ import math
 pygame.init()
 
 FPS = 60
-
 WIDTH, HEIGHT = 800, 800
 ROWS = 4
 COLS = 4
-
 RECT_HEIGHT = HEIGHT // ROWS
 RECT_WIDTH = WIDTH // COLS
-
 OUTLINE_COLOR = (187, 173, 160)
 OUTLINE_THICKNESS = 10
 BACKGROUND_COLOR = (205, 192, 180)
 FONT_COLOR = (119, 110, 101)
-
 FONT = pygame.font.SysFont("comicsans", 60, bold = True)
 MOVE_VEL = 20
-
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+
 pygame.display.set_caption("2048")
+
+def draw_grid(window):
+    for row in range(1, ROWS):
+        y = row * RECT_HEIGHT
+        pygame.draw.line(window, OUTLINE_COLOR, (0, y), (WIDTH, y), OUTLINE_THICKNESS)
+
+    for col in range(1, COLS):
+        x = col * RECT_WIDTH
+        pygame.draw.line(window, OUTLINE_COLOR, (x, 0), (x, HEIGHT), OUTLINE_THICKNESS)
+
+    pygame.draw.rect(window, OUTLINE_COLOR, (0,0, WIDTH, HEIGHT), OUTLINE_THICKNESS)
+
+def draw(window):
+    window.fill(BACKGROUND_COLOR)
+
+    draw_grid(window)
+    pygame.display.update()
 
 def main(window):
     clock = pygame.time.Clock()
@@ -35,6 +48,8 @@ def main(window):
             if event.type == pygame.QUIT:
                 run = False
                 break
+        
+        draw(window)
     
     pygame.quit()
 
